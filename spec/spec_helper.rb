c9b30422fa0d4037fa42decbaf1f9f135290eb7e
ale88andr <ale88andr@gmail.com>
2013-08-25 19:37:18 +0300

    setting up spork env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,40 +1,35 @@
 require 'rubygems'
 require 'spork'
-#uncomment the following line to use spork with the debugger
-#require 'spork/ext/ruby-debug'
 
 Spork.prefork do
-  # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   RSpec.configure do |config|
     # ## Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
+    config.mock_with :rspec
 
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
     config.use_transactional_fixtures = true
-
     config.infer_base_class_for_anonymous_controllers = false
-
     config.order = "random"
+
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true
 end
 
 Spork.each_run do
-
+  # Подгружаем файлы из директории app/
+  Dir["#{Rails.root}/app/**/*.rb"].each {|file| load file }
+  # Подгружаем файл маршрутов rails
+  load "#{Rails.root}/config/routes.rb"
+  # Перезагружаем фабрики
+  FactoryGirl.reload
 end
 
 end
