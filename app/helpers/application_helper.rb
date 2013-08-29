6b969440375280a042271988a3a439485acb4c81
ale88andr <ale88andr@gmail.com>
2013-08-29 17:44:30 +0300

    [#]+ set_title for application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+
+  APPLICATION_NAME = Rails.application.class.parent
+
 	def resource_name
 	  :user
 	end
@@ -10,4 +13,8 @@ module ApplicationHelper
 	def devise_mapping
 	  @devise_mapping ||= Devise.mappings[:user]
 	end
+
+  def set_title page_title = nil
+    page_title == '' ? APPLICATION_NAME : "#{APPLICATION_NAME} :.: #{page_title}"
+  end
 end
