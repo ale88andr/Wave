376a34fd0d47ae1ba362fabe086167edba618659
ale88andr <ale88andr@gmail.com>
2013-09-02 01:24:58 +0300

    added RecordNotFound exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
   end
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+    def record_not_found
+      render 'public/404'
+    end
 end
